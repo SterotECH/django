38bff9730e53be55faf0d4a097ca5582fb106a4a
SterotECH <agyeisterogh@gmail.com>
2022-09-15 01:05:59 +0000

    generic filtering

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,23 +1,19 @@
-import collections
+from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Count
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
+from store.filters import ProductFilter
 from .models import Collection, OrderItem, Product, Review
 from .serializer import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get("collection_id")
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {"request": self.request}
