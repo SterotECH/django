89a241a6e81f8445558e94dfbf7e68fd6420a71b
Stero tECH <agyeisterogh@gmail.com>
2022-10-28 20:28:41 +0000

    setting up railway

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -151,36 +151,18 @@ DJOSER = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 2525
-DEFAULT_FROM_EMAIL = 'from@sterobuy.com'
 
 ADMINS = [
     ('Stero', 'admin@sterobuy.com')
 ]
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'notify_customers': {
         'task': 'playground.tasks.notify_customers',
         'schedule': crontab(day_of_week=1, hour=7, minute=30),
         # 'schedule': 5,
         'args': ['Hello world'],
-    }
-}
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/2',
-        'TIMEOUT': 10 * 60,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
-        }
     }
 }
 
--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -14,3 +14,25 @@ ALLOWED_HOSTS = [
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+REDIS_URL = os.environ('REDIS_URL')
+
+CELERY_BROKER_URL = REDIS_URL
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': REDIS_URL,
+        'TIMEOUT': 10 * 60,
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    }
+}
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'localhost'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = 2525
+# DEFAULT_FROM_EMAIL = 'from@sterobuy.com'
