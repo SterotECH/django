719c4dfa681b4a56f9e57364d1f66457651998a8
Stero tECH <agyeisterogh@gmail.com>
2022-10-28 15:41:24 +0000

    fixed security issues

--- storefront/storefront/settings/prod.py
+++ storefront/storefront/settings/prod.py
@@ -7,3 +7,22 @@ DEBUG = True
 SECRET_KEY = os.environ('SECRET_KEY')
 
 ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'storefront',
+    #     'HOST': 'localhost',
+    #     'USER': 'stero',
+    #     'PASSWORD': 'code .',
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'USER': 'stero',
+        'PASSWORD': 'code .',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+}
